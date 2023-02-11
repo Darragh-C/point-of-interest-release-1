5263aeb4bfeca3767aedd0da854e4c282c71bda9
Darragh Conneely <darraghc23@gmail.com>
2023-02-11 19:02:15 +0000

    Added cookie auth strategy

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import Hapi from "@hapi/hapi";
+import Cookie from "@hapi/cookie";
 import Vision from "@hapi/vision";
 import Handlebars from "handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
+import { accountsController } from "./controllers/accounts-controller.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,6 +17,19 @@ async function init() {
     host: "localhost",
   });
   await server.register(Vision);
+  await server.register(Cookie);
+
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: "poi",
+      password: "secretpasswordnotrevealedtoanyone",
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validate: accountsController.validate,
+  });
+  server.auth.default("session");
+
   server.views({
     engines: {
       hbs: Handlebars,
