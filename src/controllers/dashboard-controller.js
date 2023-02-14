217bdfe77fdeb0793e163e6b0c868a933af665aa
Darragh Conneely <darraghc23@gmail.com>
2023-02-14 22:20:44 +0000

    Added joi schema/validation for add pin and log in

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,4 +1,5 @@
 import { db } from "../models/db.js";
+import { PinSpec } from "../models/joi-schemas.js";
 
 export const dashboardController = {
   index: {
@@ -15,6 +16,13 @@ export const dashboardController = {
   },
 
   addPin: {
+    validate: {
+      payload: PinSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("dashboard-view", { title: "Pins error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const loggedInUser = request.auth.credentials;
       const newPin = {
--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -1,10 +1,21 @@
 import Joi from "joi";
 
 export const UserSpec = {
-  firstName: Joi.string().required().max(3),
+  firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 };
 
+export const UserCredentialsSpec = {
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+};
+
+export const PinSpec = {
+  name: Joi.string().optional(),
+  description: Joi.string().optional(),
+  lattitude: Joi.string().optional(),
+  longitude: Joi.string().optional(),
+};
 
