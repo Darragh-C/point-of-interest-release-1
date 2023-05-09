93a1df63ee5f2a8ad16d2b6923950fad13a61c0a
Darragh Conneely <darraghc23@gmail.com>
2023-05-09 22:48:36 +0100

    jwt auth for user api

--- test/api/user-api-test.js
+++ test/api/user-api-test.js
@@ -1,7 +1,7 @@
 import { assert } from "chai";
 import { poiService } from "./poi-service.js";
 import { assertSubset } from "../test-utils.js";
-import { johnDoe, multiTestUsers } from "../fixtures.js";
+import { johnDoe, johnDoeCredentials, multiTestUsers } from "../fixtures.js";
 import { EventEmitter } from "events";
 
 EventEmitter.setMaxListeners(25);
@@ -10,11 +10,16 @@ const users = new Array(multiTestUsers.length);
 
 suite("User API tests", () => {
   setup(async () => {
+    poiService.clearAuth();
+    await poiService.createUser(johnDoe);
+    const response = await poiService.authenticate(johnDoeCredentials);
     await poiService.deleteAllUsers();
     for (let i = 0; i < multiTestUsers.length; i += 1) {
       // eslint-disable-next-line no-await-in-loop
       users[0] = await poiService.createUser(multiTestUsers[i]);
     }
+    await poiService.createUser(johnDoe);
+    await poiService.authenticate(johnDoeCredentials);
   });
   teardown(async () => {
   });
@@ -27,10 +32,12 @@ suite("User API tests", () => {
 
   test("delete all users", async () => {
     let returnedUsers = await poiService.getAllUsers();
-    assert.equal(returnedUsers.length, 3);
+    assert.equal(returnedUsers.length, 4);
     await poiService.deleteAllUsers();
+    await poiService.createUser(johnDoe);
+    await poiService.authenticate(johnDoeCredentials);
     returnedUsers = await poiService.getAllUsers();
-    assert.equal(returnedUsers.length, 0);
+    assert.equal(returnedUsers.length, 1);
   });
 
   test("get a user - success", async () => {
@@ -44,12 +51,14 @@ suite("User API tests", () => {
         assert.fail("Should not return a response");
       } catch (error) {
         assert(error.response.data.message === "No User with this id");
-        //assert.equal(error.response.data.statusCode, 503);
+        assert.equal(error.response.data.statusCode, 503);
       }
   });
 
   test("get a user - deleted user", async () => {
-      await poiService.deleteAllUsers();
+    await poiService.deleteAllUsers();
+    await poiService.createUser(johnDoe);
+    await poiService.authenticate(johnDoeCredentials);
       try {
         const returnedUser = await poiService.getUser(users[0]._id);
         assert.fail("Should not return a response");
